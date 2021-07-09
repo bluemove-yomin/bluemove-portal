d4fadca1b46b2b298bf51aac51da49dfba64cedd
Yomin Song <yomin@bluemove.or.kr>
2021-07-09 22:51:26 +0900

    크론잡 함수 테스트

--- applynsubmit/cronjobs.py
+++ applynsubmit/cronjobs.py
@@ -17,53 +17,59 @@ def delete_all_recruiting_data():
     # Bluemove data
     management_all_channel_id = "CV3THBHJB"
 
-    apps_notified = Applymembership.objects.filter(notified=True)
-    notis_sent = ApplymembershipNoti.objects.filter(sent=True)
-    for app in apps_notified:
-        if app.will_be_deleted_at < datetime.datetime.now():
-            app.delete()
-            app.applicant.delete()
-    for noti in notis_sent:
-        if noti.will_be_deleted_at < datetime.datetime.now():
-            client = WebClient(token=slack_bot_token)
-            try:
-                client.conversations_join(channel=management_all_channel_id)
-            except:
-                pass
-            client.chat_postMessage(
-                channel=management_all_channel_id,
-                link_names=True,
-                as_user=True,
-                blocks=[
-                    {
-                        "type": "header",
-                        "text": {
-                            "type": "plain_text",
-                            "text": f"✂️ '{noti.wanted_title}' 데이터 자동 삭제됨",
-                        },
-                    },
-                    {
-                        "type": "section",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": f"'{noti.wanted_title}' 관련 모든 데이터가 자동 삭제되었습니다.",
-                        },
-                    },
-                    {
-                        "type": "section",
-                        "fields": [
-                            {
-                                "type": "mrkdwn",
-                                "text": "*공고 ID:*\n" + noti.wanted_id,
-                            },
-                            {
-                                "type": "mrkdwn",
-                                "text": "*삭제일시:*\n"
-                                + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-                            },
-                        ],
-                    },
-                ],
-                text=f"✂️ '{noti.wanted_title}' 데이터 자동 삭제됨",
-            )
-            noti.delete()
+    from checknissue.models import Checklist
+
+    Checklist.objects.create(
+        birthday="0207", phone_last="5298", email_host="naver.com", code="1234567890"
+    )
+
+    # apps_notified = Applymembership.objects.filter(notified=True)
+    # notis_sent = ApplymembershipNoti.objects.filter(sent=True)
+    # for app in apps_notified:
+    #     if app.will_be_deleted_at < datetime.datetime.now():
+    #         app.delete()
+    #         app.applicant.delete()
+    # for noti in notis_sent:
+    #     if noti.will_be_deleted_at < datetime.datetime.now():
+    #         client = WebClient(token=slack_bot_token)
+    #         try:
+    #             client.conversations_join(channel=management_all_channel_id)
+    #         except:
+    #             pass
+    #         client.chat_postMessage(
+    #             channel=management_all_channel_id,
+    #             link_names=True,
+    #             as_user=True,
+    #             blocks=[
+    #                 {
+    #                     "type": "header",
+    #                     "text": {
+    #                         "type": "plain_text",
+    #                         "text": f"✂️ '{noti.wanted_title}' 데이터 자동 삭제됨",
+    #                     },
+    #                 },
+    #                 {
+    #                     "type": "section",
+    #                     "text": {
+    #                         "type": "mrkdwn",
+    #                         "text": f"'{noti.wanted_title}' 관련 모든 데이터가 자동 삭제되었습니다.",
+    #                     },
+    #                 },
+    #                 {
+    #                     "type": "section",
+    #                     "fields": [
+    #                         {
+    #                             "type": "mrkdwn",
+    #                             "text": "*공고 ID:*\n" + noti.wanted_id,
+    #                         },
+    #                         {
+    #                             "type": "mrkdwn",
+    #                             "text": "*삭제일시:*\n"
+    #                             + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+    #                         },
+    #                     ],
+    #                 },
+    #             ],
+    #             text=f"✂️ '{noti.wanted_title}' 데이터 자동 삭제됨",
+    #         )
+    #         noti.delete()
